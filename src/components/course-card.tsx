0de97ebd93176ecb931b30df4156434bdc55172e
Kyrylo Lolenko <kir260302@gmail.com>
2024-12-05 10:04:52 +0200

    Add Progress bar

--- src/components/course-card.tsx
+++ src/components/course-card.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import IconBadge from "@/components/icon-badge";
 import { BookOpen } from "lucide-react";
 import { formatPrice } from "@/lib/format";
+import { CourseProgress } from "./course-progress";
 
 interface CourseCardProps {
   id: string;
@@ -45,7 +46,11 @@ function CourseCard({
             </div>
           </div>
           {progress !== null ? (
-            <div>Todo: Progress Component</div>
+            <CourseProgress
+              variant={progress === 100 ? "success" : "default"}
+              size="sm"
+              value={progress}
+            />
           ) : (
             <p className="text-md md:text-sm font-medium">
               {formatPrice(price)}
