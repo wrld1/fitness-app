4cde2746af51e203bab5e70c2d994ea741c8268f
Kyrylo Lolenko <kir260302@gmail.com>
2024-12-06 22:35:36 +0200

    Change currency in chartbar

--- src/app/(dashboard)/dashboard/teacher/analytics/_components/chart.tsx
+++ src/app/(dashboard)/dashboard/teacher/analytics/_components/chart.tsx
@@ -27,7 +27,7 @@ export function Chart({ data }: ChartProps) {
             fontSize={12}
             tickLine={false}
             axisLine={false}
-            tickFormatter={(value) => `грн ${value}`}
+            tickFormatter={(value) => `₴${value}`}
           />
           <Bar dataKey="total" fill="#0369a1" radius={[4, 4, 0, 0]} />
         </BarChart>
